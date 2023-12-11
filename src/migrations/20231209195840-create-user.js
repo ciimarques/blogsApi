d414d0a334b2e05a993ded7df379281cd364c7f4
Cintia Marques <cintiamarques.mk@gmail.com>
2023-12-11 12:06:16 -0300

    Req1

---
+++ src/migrations/20231209195840-create-user.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+   await queryInterface.createTable('users' , {
+    id: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    display_name: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    email: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      unique: true
+    },
+    password: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    image: {
+      type: Sequelize.STRING,
+    }
+   })
+  },
+
+  async down (queryInterface, Sequelize) {
+    
+    await queryInterface.dropTable('users');
+  }
+};
---
+++ src/migrations/20231211144614-create_blog_posts.js
@@ -0,0 +1,46 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+   await queryInterface.createTable('blog_posts' , {
+    id: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    title: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    content: {
+      type: Sequelize.TEXT,
+      allowNull: false,
+    },
+    user_id: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'users', 
+        key: 'id',       
+      }
+    },
+    image: {
+      type: Sequelize.STRING,
+    },
+    published: {
+      type: Sequelize.DATE,
+      allowNull: false
+    },
+    updated: {
+      type: Sequelize.DATE,
+      allowNull: false
+    },
+   })
+  },
+
+  async down (queryInterface, Sequelize) {
+    
+    await queryInterface.dropTable('blog_posts');
+  }
+};
